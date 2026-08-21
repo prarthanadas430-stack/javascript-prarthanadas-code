4c5e444f1b49e56d2407524be8653e173bd12d14
Prarthana Das <prarthanadas430@gmail.com>
2026-08-21 18:20:11 +0530

    Add files via upload

---
+++ code2.js
@@ -0,0 +1,9 @@
+const account = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "123456"
+let accountCity = "Kolkata"
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bengaluru"
+console.log(account);
+console.table([account, accountEmail, accountPassword, accountCity]);
