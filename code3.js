f76bd9acb46d4dc7b193dd4904f8212c59a80caf
Prarthana Das <prarthanadas430@gmail.com>
2026-08-22 16:07:14 +0530

    Add files via upload

---
+++ code3.js
@@ -0,0 +1,11 @@
+"use strict"
+console.log(3+3)
+console.log("Prarthana")
+let name = "Prarthana"
+let age = 18
+let isLoggedIn = false
+let state;
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof undefined);
+console.log(typeof null);
